20ce4728f01a807b48b26bd7e0c5c539c055ed95
Mozart96P <mozart96p@hotmail.com>
2021-08-31 15:11:07 -0300

    O codigo em JS apresenta falhas. Nao esta funcionando

--- interacao.js
+++ interacao.js
@@ -1,6 +1,21 @@
-var bi1 
-var bi2
-var bi3
-var bi4
+// Média aritmética.
+// Soma de todos os elementos(Todas as notas bimestrais), dividindo-se pelo número delas mesmas.
 
-var media = (bi1 + bi2 + bi3 + bi4) / 4;
+var inputnota1 = document.getElementById("nota1");
+var inputnota2 = document.getElementById("nota2");
+var inputnota3 = document.getElementById("nota3");
+var inputnota4 = document.getElementById("nota4");
+
+var resultado = document.getElementById("resultado");
+
+function calcular(){
+
+                var bimestreum = inputnota1.value;
+                var bimestredois = inputnota2.value;
+                var bimestretres = inputnota3.value;
+                var bimestrequatro = inputnota4.value;
+
+                var somanotas = bimestreum + bimestredois + bimestretres +bimestrequatro / 4;
+
+                console.log(somanotas);
+}

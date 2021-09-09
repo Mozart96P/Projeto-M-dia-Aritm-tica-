2eeb658af8e81e0b3690f1c8c7422498f59cf312
Mozart96P <mozart96p@hotmail.com>
2021-09-09 15:35:48 -0300

    Projeto finalizado

--- interacao.js
+++ interacao.js
@@ -1,23 +1,30 @@
 // Média aritmética.
 // Soma de todos os elementos(Todas as notas bimestrais), dividindo-se pelo número delas mesmas.
 
-var inputnota1 = document.getElementById("nota1");
-var inputnota2 = document.getElementById("nota2");
-var inputnota3 = document.getElementById("nota3");
-var inputnota4 = document.getElementById("nota4");
+function calcular() {
+    // se tu nao pegar o value tu nao tem o conteúdo experimente remover 
+    // o value de uma das linhas abaixo e executar
+    // vai imprimir no console log a linha de comando tu vai notar 
+    // já passei direto pra float pq economiza variaveis
+    
+    let inputnota1 = parseFloat(document.getElementById("nota1").value);
+    let inputnota2 = parseFloat(document.getElementById("nota2").value);
+    let inputnota3 = parseFloat(document.getElementById("nota3").value);
+    let inputnota4 = parseFloat(document.getElementById("nota4").value);
+    let resultado = document.getElementById("resultado");
+    
+    // aqui só pra tu ver como imprime
+    console.log(inputnota1)
+    console.log(inputnota2)
+    console.log(inputnota3)
+    console.log(inputnota4)
+   
+    // aqui ja nomei direto as variaveis
+    let somanotas = (inputnota1 + inputnota2 + inputnota3 + inputnota4) / 4;
 
-var resultado = document.getElementById("resultado");
-
-function calcular(){
-
-                var bimestreum = inputnota1.value;
-                var bimestredois = inputnota2.value;
-                var bimestretres = inputnota3.value;
-                var bimestrequatro = inputnota4.value;
-
-                var somanotas = (bimestreum + bimestredois + bimestretres +bimestrequatro) / 4;
-
-                console.log(somanotas);
-
-                resultado.innerHTML`<p> Sua Média é , ${somanotas} .</p>`
+    console.log(somanotas);
+    //aqui faltou o = 
+    // e tu tem que formatar como vai sair pq ta só um texto simples 
+    resultado.innerHTML = `<p> A média do Zé é ,${somanotas} .</p>` 
 }
+
